345295993e84d8e4722902bc375ced71df11c8ff
Vladimir Teplyakov <vteplakov@gmail.com>
2025-08-18 19:55:36 +0300

    Добавил тест заполненный логин и пустой пароль

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -34,5 +34,8 @@ class LoginPageHelper(BasePage):
     def click_login(self):
         self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
 
+    def send_login(self):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys("dicky@mail.ru")
+
     def get_error_text(self):
         return self.find_element(LoginPageLocators.ERROR_TEXT).text
--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -4,9 +4,18 @@ from pages.LoginPage import LoginPageHelper
 
 BASE_URL = 'https://ok.ru/'
 EMPTY_LOGIN_ERROR = 'Введите логин'
+EMPTY_PASSWORD_ERROR = 'Введите пароль'
 
 def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
+
+def test_login_and_empty_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.send_login()
+    LoginPage.click_login()
+    assert LoginPage.get_error_text() == EMPTY_PASSWORD_ERROR
+
