0048478bb535193a77be6db5ebf89bbc039adfaf
Vladimir Teplyakov <vteplakov@gmail.com>
2025-08-25 20:39:59 +0300

    Добавил шаги и заголовки Allure

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,11 @@ from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
 import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 
 BASE_URL = 'https://ok.ru/'
 EMPTY_LOGIN_ERROR = 'Введите логин'
@@ -17,11 +22,10 @@ def test_empty_login_and_password(browser):
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
-@allure.title("Проверка")
+@allure.title("Проверка ошибки при заполненном поле Логин и пустом поле Пароль")
 def test_login_and_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.send_login()
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_PASSWORD_ERROR
-
